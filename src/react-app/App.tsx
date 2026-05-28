7a43c40e966e5eb8bc223252f1cdf87437a78b73
MacCormick <jmac@dickinson.edu>
2026-05-28 17:17:19 -0400

    Add hero logo and update styles in App.css

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import dnuLogo from "./assets/dnu-logo.svg";
 
 function App() {
 	return (
 		<>
+			<img className="hero-logo" src={dnuLogo} alt="DNU Lab logo" />
 			<h1>DNU Lab</h1>
 			<h2>deep network understanding</h2>
 			<h3>Dickinson College</h3>
